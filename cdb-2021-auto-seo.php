c63eadf86805bf286f0bc37fff55603d72a5fa7c
Carl Brubaker <developing.rust@gmail.com>
2021-10-14 11:01:20 -0400

    Added Readme

--- cdb-2021-auto-seo.php
+++ cdb-2021-auto-seo.php
@@ -5,15 +5,14 @@
  * Plugin URI: The home page of the plugin, which should be a unique URL, preferably on your own website. This must be unique to your plugin. You cannot use a WordPress.org URL here.
  * Description: Automatically adds SEO tags to <head>. Does not display any field inputs in WordPress Editor. name="description" can be edited through post excerpts and taxonomy descriptions.
  * Version: 1.0.0
- * Requires at least: 5.8.1
+ * Requires at least: 5.8
  * Requires PHP: 7.4
  * Author: Carl David Brubaker
- * Author URI: The author’s website or profile on another website, such as WordPress.org.
+ * Author URI: https://github.com/Spleeding1
  * License: GPLv2 (or later)
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: CDB_2021_AUTO_SEO
- * Domain Path: /languages    The domain path lets WordPress know where to find the translations. More information can be found in the Domain Path section of the How to Internationalize your Plugin page.
- * Network: Whether the plugin can only be activated network-wide. Can only be set to true, and should be left out when not needed.
+ * Domain Path: /languages
  */
 
 namespace cdb_2021_Auto_SEO;
